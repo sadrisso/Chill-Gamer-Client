3d370344cfa54e76e697ae0f3055dbce23350180
sadrisso <sadriisso304@gmail.com>
2024-12-03 18:49:19 +0600

    first commit with project setup

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+
+
+const MainLayout = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+        </div>
+    );
+};
+
+export default MainLayout;
